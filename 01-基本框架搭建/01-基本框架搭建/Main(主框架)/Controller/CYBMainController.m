777eb780294f89dde65089a4d8a71dff522dab16
rrljiyotoun <yichuji1983329@126.com>
2021-04-12 23:46:30 +0800

    添加了技术点

--- 01-基本框架搭建/01-基本框架搭建/Main(主框架)/Controller/CYBMainController.m
+++ 01-基本框架搭建/01-基本框架搭建/Main(主框架)/Controller/CYBMainController.m
@@ -19,6 +19,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    /*
+     技术点：1,把系统自带的tabBar删除掉，换上自定义的view，自定义的view的frame等于系统tabBar的frame
+            2，自定义按钮，通过for循环添加到自定义的view上，每个按钮的宽等于自定义view的 0.2 同时为每一个按钮设置背景图片
+            3，给按钮添加点击事件
+     */
    
     //替换系统的 tabBar
     [self.tabBar removeFromSuperview];
@@ -29,8 +34,6 @@
     for (int i = 0; i < 5; i ++) {
         UIButton *tabBarBtn = [UIButton buttonWithType:UIButtonTypeCustom];
 
-        
- 
         //未选中时的背景
         NSString *BtnIcon = [NSString stringWithFormat:@"TabBar%d",i + 1];
         [tabBarBtn setBackgroundImage:[UIImage imageNamed:BtnIcon] forState:UIControlStateNormal];
