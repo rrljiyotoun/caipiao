4dbc20ff4e17ca149ad3a3569f3ddb976a6f987c
rrljiyotoun <yichuji1983329@126.com>
2021-04-12 18:23:53 +0800

    基本框架第一步

---
+++ 01-基本框架搭建/01-基本框架搭建/MyLottery(我的彩票)/Controller/CYBMyLotteryController.m
@@ -0,0 +1,37 @@
+//
+//  CYBMyLotteryController.m
+//  01-基本框架搭建
+//
+//  Created by apple on 16/8/21.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "CYBMyLotteryController.h"
+
+@interface CYBMyLotteryController ()
+
+@end
+
+@implementation CYBMyLotteryController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
